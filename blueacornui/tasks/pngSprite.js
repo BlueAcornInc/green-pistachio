4a7380345d64043f1795d310db34e73811a7652a
Greg Harvell <greg@blueacorn.com>
2020-03-16 12:53:54 -0400

    Updating contributors

--- blueacornui/tasks/pngSprite.js
+++ blueacornui/tasks/pngSprite.js
@@ -2,13 +2,10 @@
  * @package     BlueAcorn/GreenPistachio
  * @version     4.0.0
  * @author      Blue Acorn iCi <code@blueacorn.com>
- * @author      Greg Harvell <greg@blueacorn.com>
- * @author      Michael Bottens <michael.bottens@blueacorn.com>
  * @copyright   Copyright © Blue Acorn iCi. All rights reserved.
  */
 
 import path from 'path';
-import merge from 'merge-stream';
 import {
     src,
     dest,
