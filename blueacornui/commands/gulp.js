bff287c8fe873beb82e037e47de2260ab98bac1c
Michael Bottens <michael.bottens@blueacorn.com>
2020-04-15 09:04:32 -0400

    logging update

--- blueacornui/commands/gulp.js
+++ blueacornui/commands/gulp.js
@@ -1,27 +1,24 @@
 import isInstalled from '../helpers/is-installed';
+const log = require('gulplog');
+const logEvents = require('gulp-cli/lib/versioned/^4.0.0/log/events');
+const logSyncTask = require('gulp-cli/lib/versioned/^4.0.0/log/sync-task');
+const toConsole = require('gulp-cli/lib/shared/log/to-console');
 
 export default async (program) => {
     const installed = await isInstalled();
     if (installed) {
         const gulp = require('gulp');
+        logEvents(gulp);
+        logSyncTask(gulp);
+        toConsole(log, {});
         const namedTasks = require('../../gulpfile.babel');
 
         for (const namedTask of Object.keys(namedTasks)) {
             program
                 .command(namedTask)
                 .action(() => {
-                    namedTasks[namedTask]();
+                    gulp.series(namedTasks[namedTask], (done) => done())();
                 });
         }
-        
-        // const tasks = gulp.tasks ? Object.keys(gulp.tasks).sort() : gulp.tree().nodes.sort();
-
-        // for (const task of tasks) {
-        //     program
-        //         .command(task)
-        //         .action(() => {
-        //             gulp.task(task)();
-        //         });
-        // }
     }
 };
